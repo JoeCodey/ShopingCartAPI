875870a9cc262c38343e27038276479be1ae5439
Joseph lefebvre <jlefe074@uottawa.ca>
2019-01-20 14:53:44 -0500

    Initial setup commit, including set-up of express, mongodb, and mlab (database URI should be contained in heroku env variables, it should not be published to github for security reasons)

---
+++ app.js
@@ -0,0 +1,52 @@
+const mongodb = require('mongodb') ;
+
+
+let seedData = [
+  {
+    Title: 'Bicycle',
+    price: 200,
+    quantity: 2
+  },
+  {
+    Title: 'Helmet',
+    price: 50,
+    quantity: 5
+  },
+  {
+    Title: 'Knee pads',
+    price: 25,
+    quantity: 10
+  }
+]
+
+
+
+console.log(process.env.MONGOLAB_URI) ;
+//
+// mongodb.MongoClient.connect(uri, function(err, client) {
+//
+//
+//   if(err) throw err;
+//
+//   /*
+//    * Get the database from the client. Nothing is required to create a
+//    * new database, it is created automatically when we insert.
+//    */
+//
+//   let db = client.db('shopping_api') ;
+//
+//   /*
+//    * First we'll add a few songs. Nothing is required to create the
+//    * songs collection; it is created automatically when we insert.
+//    */
+//
+//   let products = db.collection('products');
+//
+//   songs.insert(seedData, function(err, result) {
+//
+//    if(err) throw err;
+//
+//  });
+//
+//
+// });
